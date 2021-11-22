3f062f99ae070ba366c55a79c6e881a4778a9ecc
vinny <vinnygeoffrey@gmail.com>
2021-11-21 21:28:44 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Snake and Ladder Game Board
+
+The goal is to make a game board table that matches the following image (minus the snakes and ladders): <br><br>
+
+![board-screenshot](./assets/images/snakes-ladders-table.png)
+<br><br>
+## References:
+
+Author: Amie Carroll
---
+++ index.js
@@ -0,0 +1,33 @@
+const showDie = document.getElementById("die-image");
+const rollDie = document.getElementById("btn-roll");
+
+function createTable() {
+  let table = document.getElementById("myTable");
+
+  // 10 rows
+  for (let i = 0; i < 10; i++) {
+    let row = table.insertRow(i);
+    row.className = "row-direction";
+
+    // 10 columns
+    for (let j = 0; j < 10; j++) {
+      let cell = row.insertCell(j);
+      cell.innerHTML = i * 10 + j + 1; //start with 1 instead of 0
+      cell.className = "item";
+    }
+  }
+};
+
+function makeMove() {
+  // random # between 1 and 6
+  let rollValue = Math.floor(Math.random() * 6) + 1;
+  console.log(rollValue);
+
+  // display die image
+  showDie.style.display = "block";
+  showDie.src = "./assets/images/" + rollValue + ".png";
+  console.log(showDie.src);
+};
+
+document.getElementById("btn-roll").addEventListener("click", makeMove);
+document.getElementById("btn-table").addEventListener("click", createTable);
